c3afd849debe0ef4dec4acbd7e95ca5911900d3d
Ryankids <theravenkids@gmail.com>
2016-01-06 22:22:13 +0800

    Create MajorAssignment.c

---
+++ MajorAssignment.c
@@ -0,0 +1,17 @@
+#include "MajorAssignment.h"
+
+int main(){
+
+        Allocate_Memory();
+
+        LoadFile();
+        //Init();
+        //Send_To_Device();
+
+        //Get_From_Device();
+
+        //Save_Data();
+
+        Free_Memory();
+        return 0;
+}
