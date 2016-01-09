2518f8feba3c913684e97a80816ec265291ffd4b
Ryankids <theravenkids@gmail.com>
2016-01-09 17:07:37 +0800

    Update Major_CPU_test.c

--- Major_CPU_test.c
+++ Major_CPU_test.c
@@ -372,7 +372,7 @@ void CPUHeatContactFunction() {
 				if (!h_body[i + (j - 1)*NX + k*NX*NY]) {
 					for (z = 0; z < 5; z++) {
 						if(z == 0 || z == 4){
-						FB[i + j*NX + k*NX*NY + z*N] = E[i + j*NX + k*NX*NY + z*N];
+						FB[i + j*NX + k*NX*NY + z*N] = F[i + j*NX + k*NX*NY + z*N];
 						}
 						else{
 						FB[i + j*NX + k*NX*NY + z*N] = (-2)*speed*U[i + j*NX + k*NX*NY + z*N];
@@ -382,7 +382,7 @@ void CPUHeatContactFunction() {
 				if (!h_body[i + (j + 1)*NX + k*NX*NY]) {
 					for (z = 0; z < 5; z++) {
 						if(z == 0 || z == 4){
-						FF[i + j*NX + k*NX*NY + z*N] = E[i + j*NX + k*NX*NY + z*N];
+						FF[i + j*NX + k*NX*NY + z*N] = F[i + j*NX + k*NX*NY + z*N];
 						}
 						else{
 						FF[i + j*NX + k*NX*NY + z*N] = (-2)*speed*U[i + j*NX + k*NX*NY + z*N];
@@ -392,7 +392,7 @@ void CPUHeatContactFunction() {
 				if (!h_body[i + j*NX + (k - 1)*NX*NY]) {
 					for (z = 0; z < 5; z++) {
 						if(z == 0 || z == 4){
-						FD[i + j*NX + k*NX*NY + z*N] = E[i + j*NX + k*NX*NY + z*N];
+						FD[i + j*NX + k*NX*NY + z*N] = G[i + j*NX + k*NX*NY + z*N];
 						}
 						else{
 						FD[i + j*NX + k*NX*NY + z*N] = (-2)*speed*U[i + j*NX + k*NX*NY + z*N];
@@ -402,7 +402,7 @@ void CPUHeatContactFunction() {
 				if (!h_body[i + j*NX + (k + 1)*NX*NY]) {
 					for (z = 0; z < 5; z++) {
 						if(z == 0 || z == 4){
-						FU[i + j*NX + k*NX*NY + z*N] = E[i + j*NX + k*NX*NY + z*N];
+						FU[i + j*NX + k*NX*NY + z*N] = G[i + j*NX + k*NX*NY + z*N];
 						}
 						else{
 						FU[i + j*NX + k*NX*NY + z*N] = (-2)*speed*U[i + j*NX + k*NX*NY + z*N];
